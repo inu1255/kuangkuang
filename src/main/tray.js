633131ac9a03c219bd4f30b8cfea8f3b1f7c7166
inu1255 <929909260@qq.com>
2018-03-09 11:48:57 +0800

    0.0.1

--- src/main/tray.js
+++ src/main/tray.js
@@ -5,5 +5,13 @@ app.on('ready', () => {
     tray = new Tray(nativeImage.createFromPath('./build/icons/256x256.png').resize({ width: 18, height: 18 }));
     tray.on('click', () => {
         app.mainWindow.isVisible() ? app.mainWindow.hide() : app.mainWindow.show();
-    });
+	});
+	const contextMenu = Menu.buildFromTemplate([{
+		label: "退出",
+		click(){
+			app.quit();
+		}
+	}]);
+    tray.setToolTip('挖钱宝');
+    tray.setContextMenu(contextMenu);
 });
